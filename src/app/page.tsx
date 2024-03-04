9a8eb74ac62b8fd4d83999d6de6ca409cef3d933
apollo11silva <apollo11silva@gmail.com>
2024-03-04 18:03:13 -0300

    configurar carrocel e script de mapa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+// /home/apollo/Documentos/projetos_javascript/EuMimoMesmo/src/app/page.tsx
 /* eslint-disable @next/next/no-img-element */
 
 import MaxWidthWrapper from "@/components/MaxWidthWrapper"
@@ -62,11 +63,13 @@ export default function Home() {
           <div className='flex flex-col sm:flex-row gap-4 mt-6'>
             <Button variant="ghost" className="hover:bg-pink-50">Confira nossa qualidade &rarr;</Button>
           </div>
-          <div className="size-2/3 ">
-            <Carousel autoSlide={true} autoSlideInterval={3000} slides={slides} />
-          </div>
+          
         </div>
       </MaxWidthWrapper>
+
+      <div className="w-full">
+            <Carousel autoSlide={true} autoSlideInterval={3000} slides={slides} />
+          </div>
 
       <svg  data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" ><path  d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill" fill="#FDF2F8" fillOpacity="1"></path>
       </svg>
