4731442241321d2b3fde4ee54301a5c7ba177375
apollo11silva <apollo11silva@gmail.com>
2024-03-05 14:47:36 -0300

    icone whatsapp

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+// /home/apollo/Documentos/projetos_javascript/EuMimoMesmo/src/components/Footer.tsx
 'use client'
 
 import { usePathname } from 'next/navigation'
@@ -10,6 +11,9 @@ import Image from 'next/image'
 import siteProtegido from '../../public/siteProtegido.png'
 
 import googleSecurity from '../../public/googleSecurity.png'
+
+import whatsappIcon from '../../public/whatsapp_icon.png'
+
 const Footer = () => {
   
   const pathname = usePathname()
@@ -20,74 +24,92 @@ const Footer = () => {
   ]
   
   return (
-    <footer className='bg-white flex-grow-0'>
-      <MaxWidthWrapper>
-        <div className='border-t border-gray-200'>
-          {pathsToMinimize.includes(pathname) ? null : (
-            <div className='pb-8 pt-16'>
-              <div className='flex justify-center'>
-                <Icons.logo className='h-12 w-auto' />
-              </div>
-            </div>
-          )}
-
-          {pathsToMinimize.includes(pathname) ? null : (
-            <div>
-              <div className='relative flex items-center px-6 py-6 sm:py-8 lg:mt-0'>
-                <div className='absolute inset-0 overflow-hidden rounded-lg'>
-                  <div
-                    aria-hidden='true'
-                    className='absolute bg-pink-50 inset-0 bg-gradient-to-br bg-opacity-90'
-                  />
+    <>
+      <footer className='bg-white flex-grow-0'>
+        <MaxWidthWrapper>
+          <div className='border-t border-gray-200'>
+            {pathsToMinimize.includes(pathname) ? null : (
+              <div className='pb-8 pt-16'>
+                <div className='flex justify-center'>
+                  <Icons.logo className='h-12 w-auto' />
                 </div>
+              </div>
+            )}
 
+            {pathsToMinimize.includes(pathname) ? null : (
+              <div>
+                <div className='relative flex items-center px-6 py-6 sm:py-8 lg:mt-0'>
+                  <div className='absolute inset-0 overflow-hidden rounded-lg'>
+                    <div
+                      aria-hidden='true'
+                      className='absolute bg-pink-50 inset-0 bg-gradient-to-br bg-opacity-90'
+                    />
+                  </div>
+
+                </div>
+              </div>
+            )}
+          </div>
+
+          <div className='py-10 md:flex md:items-center md:justify-between'>
+            <div className='text-center md:text-left'>
+              <p className='text-sm text-muted-foreground'>
+                &copy; {new Date().getFullYear()} Todos os direitos reservados
+              </p>
+            </div>
+
+            <div className='mt-4 flex items-center justify-center md:mt-0'>
+              <div className='flex space-x-8'>
+                <Link
+                  href='#'
+                  className='text-sm text-muted-foreground hover:text-gray-600'>
+                  Termos
+                </Link>
+                <Link
+                  href='#'
+                  className='text-sm text-muted-foreground hover:text-gray-600'>
+                  Política de privacidade
+                </Link>
+                <Link
+                  href='#'
+                  className='text-sm text-muted-foreground hover:text-gray-600'>
+                  Política de cookies
+                </Link>
+                <Link
+                  href="https://transparencyreport.google.com/safe-browsing/search?url=eumimomesmo.com.br&hl=pt"
+                  target="_blank"
+                >
+                  <Image height={25} width={225} src={googleSecurity} alt="ceritficado de segurança"/>
+                </Link>
+                
+                <Link
+                  href="" //TODO
+                  target="_blank"
+                >
+                  <Image height={25} width={190} src={siteProtegido} alt="ceritficado de segurança SSL"/>
+                </Link>
               </div>
             </div>
-          )}
-        </div>
-
-        <div className='py-10 md:flex md:items-center md:justify-between'>
-          <div className='text-center md:text-left'>
-            <p className='text-sm text-muted-foreground'>
-              &copy; {new Date().getFullYear()} Todos os direitos reservados
-            </p>
           </div>
+        </MaxWidthWrapper>
+      </footer>
 
-          <div className='mt-4 flex items-center justify-center md:mt-0'>
-            <div className='flex space-x-8'>
-              <Link
-                href='#'
-                className='text-sm text-muted-foreground hover:text-gray-600'>
-                Termos
-              </Link>
-              <Link
-                href='#'
-                className='text-sm text-muted-foreground hover:text-gray-600'>
-                Política de privacidade
-              </Link>
-              <Link
-                href='#'
-                className='text-sm text-muted-foreground hover:text-gray-600'>
-                Política de cookies
-              </Link>
-              <Link
-                href="https://transparencyreport.google.com/safe-browsing/search?url=eumimomesmo.com.br&hl=pt"
-                target="_blank"
-              >
-                <Image height={25} width={225} src={googleSecurity} alt="ceritficado de segurança"/>
-              </Link>
-              
-              <Link
-                href="" //TODO
-                target="_blank"
-              >
-                <Image height={25} width={190} src={siteProtegido} alt="ceritficado de segurança SSL"/>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </MaxWidthWrapper>
-    </footer>
+     {/* Ícone do WhatsApp */}
+     <a
+        href="https://wa.me/556198856142?text=Olá!%20Gostaria%20de%20mais%20informações."
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{
+          position: 'fixed',
+          bottom: '20px',
+          right: '20px',
+          zIndex: 1000,
+        }}
+      >
+        <Image src={whatsappIcon} alt="WhatsApp" width={60} height={60} />
+      </a>           
+
+    </>
   )
 }
 
