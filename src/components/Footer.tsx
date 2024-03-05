c8cbc0c152969325c0471746f8eac7fbc531f3c3
JVSantosSilva <joaovic1004@gmail.com>
2024-03-05 15:18:33 -0300

    footer whatsapp + coluna de termos

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -22,40 +22,71 @@ const Footer = () => {
   return (
     <>
       <footer className='bg-white flex-grow-0'>
-        <MaxWidthWrapper>
-          <div className='border-t border-gray-200'>
-            {pathsToMinimize.includes(pathname) ? null : (
-              <div className='pb-8 pt-16'>
-                <div className='flex justify-center'>
-                  <Icons.logo className='h-12 w-auto' />
+      <MaxWidthWrapper>
+        <div className='border-t border-gray-200'>
+          {pathsToMinimize.includes(pathname) ? null : (
+            <div className='pb-8 pt-16'>
+              <div className='flex justify-center'>
+                <Icons.logo className='h-12 w-auto' />
+              </div>
+            </div>
+          )}
+          {pathsToMinimize.includes(pathname) ? null : (
+            <div>
+              <div className='relative flex items-center px-6 py-6 sm:py-8 lg:mt-0'>
+                <div className='absolute inset-0 overflow-hidden rounded-lg'>
+                  <div
+                    aria-hidden='true'
+                    className='absolute bg-pink-50 inset-0 bg-gradient-to-br bg-opacity-90'
+                  />
                 </div>
               </div>
-            )}
-          </div>
-
-          <div className='py-10 md:flex md:items-center md:justify-between'>
-            <div className='text-center md:text-left'>
-              <p className='text-sm text-muted-foreground'>
-                &copy; {new Date().getFullYear()} Todos os direitos reservados
-              </p>
             </div>
+          )}
+        </div>
 
-            <div className='mt-4 flex items-center justify-center md:mt-0'>
-              <div className='flex space-x-8'>
-                <Link href='#' className='text-sm text-muted-foreground hover:text-gray-600'>Termos</Link>
-                <Link href='#' className='text-sm text-muted-foreground hover:text-gray-600'>Política de privacidade</Link>
-                <Link href='#' className='text-sm text-muted-foreground hover:text-gray-600'>Política de cookies</Link>
-                <Link href="https://transparencyreport.google.com/safe-browsing/search?url=eumimomesmo.com.br&hl=pt" target="_blank">
-                  <Image height={25} width={225} src={googleSecurity} alt="certificado de segurança"/>
-                </Link>
-                <Link href="" target="_blank"> {/* Atualize com o link desejado */}
-                  <Image height={25} width={190} src={siteProtegido} alt="certificado de segurança SSL"/>
-                </Link>
-              </div>
+        <div className='py-10 md:flex md:items-center md:justify-between'>
+          <div className='text-center flex flex-col md:text-left'>
+            <Link
+              href='#'
+              className='text-sm text-muted-foreground hover:text-gray-600'>
+              Termos
+            </Link>
+            <Link
+              href='#'
+              className='text-sm text-muted-foreground hover:text-gray-600'>
+              Política de privacidade
+            </Link>
+            <Link
+              href='#'
+              className='text-sm text-muted-foreground hover:text-gray-600'>
+              Política de cookies
+            </Link>
+          </div>
+          <div className="text-center ">
+            <p className='text-sm text-muted-foreground'>
+              &copy; {new Date().getFullYear()} Todos os direitos reservados
+            </p>
+          </div>
+          <div className='mt-4 flex items-center justify-center md:mt-0'>
+            <div className='flex space-x-8'>
+              <Link
+                href="https://transparencyreport.google.com/safe-browsing/search?url=eumimomesmo.com.br&hl=pt"
+                target="_blank"
+              >
+                <Image height={25} width={225} src={googleSecurity} alt="ceritficado de segurança"/>
+              </Link>
+              <Link
+                href="" //TODO
+                target="_blank"
+              >
+                <Image height={25} width={190} src={siteProtegido} alt="ceritficado de segurança SSL"/>
+              </Link>
             </div>
           </div>
-        </MaxWidthWrapper>
-      </footer>
+        </div>
+      </MaxWidthWrapper>
+    </footer>
 
       {/* Ícone do WhatsApp */}
       <a href="https://wa.me/556198856142?text=Olá!%20Gostaria%20de%20mais%20informações." target="_blank" rel="noopener noreferrer" style={{
