fee456683c4385dde046dade698db270828b4ab6
JVSantosSilva <joaovic1004@gmail.com>
2024-03-04 21:10:33 -0300

    footer em formato de coluna

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -30,7 +30,6 @@ const Footer = () => {
               </div>
             </div>
           )}
-
           {pathsToMinimize.includes(pathname) ? null : (
             <div>
               <div className='relative flex items-center px-6 py-6 sm:py-8 lg:mt-0'>
@@ -40,43 +39,40 @@ const Footer = () => {
                     className='absolute bg-pink-50 inset-0 bg-gradient-to-br bg-opacity-90'
                   />
                 </div>
-
               </div>
             </div>
           )}
         </div>
 
         <div className='py-10 md:flex md:items-center md:justify-between'>
-          <div className='text-center md:text-left'>
+          <div className='text-center flex flex-col md:text-left'>
+            <Link
+              href='#'
+              className='text-sm text-muted-foreground hover:text-gray-600'>
+              Termos
+            </Link>
+            <Link
+              href='#'
+              className='text-sm text-muted-foreground hover:text-gray-600'>
+              Política de privacidade
+            </Link>
+            <Link
+              href='#'
+              className='text-sm text-muted-foreground hover:text-gray-600'>
+              Política de cookies
+            </Link>
             <p className='text-sm text-muted-foreground'>
               &copy; {new Date().getFullYear()} Todos os direitos reservados
             </p>
           </div>
-
           <div className='mt-4 flex items-center justify-center md:mt-0'>
             <div className='flex space-x-8'>
-              <Link
-                href='#'
-                className='text-sm text-muted-foreground hover:text-gray-600'>
-                Termos
-              </Link>
-              <Link
-                href='#'
-                className='text-sm text-muted-foreground hover:text-gray-600'>
-                Política de privacidade
-              </Link>
-              <Link
-                href='#'
-                className='text-sm text-muted-foreground hover:text-gray-600'>
-                Política de cookies
-              </Link>
               <Link
                 href="https://transparencyreport.google.com/safe-browsing/search?url=eumimomesmo.com.br&hl=pt"
                 target="_blank"
               >
                 <Image height={25} width={225} src={googleSecurity} alt="ceritficado de segurança"/>
               </Link>
-              
               <Link
                 href="" //TODO
                 target="_blank"
